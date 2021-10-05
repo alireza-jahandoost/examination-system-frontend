f5185d7ddbe4fe6abc6ec27fac08b3cb5f1e9894
alireza <alireza.jhd2000@gmail.com>
2021-10-05 08:04:50 +0330

    Style Notification Alert

--- src/components/notification-alert/notification-alert.component.jsx
+++ src/components/notification-alert/notification-alert.component.jsx
@@ -1,7 +1,17 @@
-import { Alert } from "react-bootstrap";
+import { Row, Col, Alert } from "react-bootstrap";
 
 const NotificationAlert = ({ children, variant }) => {
-  return <Alert variant={variant}>{children}</Alert>;
+  return (
+    <div className="fixed-bottom">
+      <Row>
+        <Col lg={6} xs={12}>
+          <Alert dismissible variant={variant}>
+            {children}
+          </Alert>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default NotificationAlert;
