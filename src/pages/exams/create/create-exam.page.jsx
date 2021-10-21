8b75c498717fed11cf1c208905616ce0aac11ee9
alireza <alireza.jhd2000@gmail.com>
2021-10-21 19:24:55 +0330

    Move create-exam form to its component

--- src/pages/exams/create/create-exam.page.jsx
+++ src/pages/exams/create/create-exam.page.jsx
@@ -1,105 +1,14 @@
-import { useState } from "react";
-import { Container, Button, Row, Col, Form } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import Sidebar from "../../../components/sidebar/sidebar.component";
-
-import TextInput from "../../../components/inputs/text-input.component";
-import NumberInput from "../../../components/inputs/number-input.component";
-import TextareaInput from "../../../components/inputs/textarea-input.component";
-import CheckboxInput from "../../../components/inputs/checkbox-input.component";
+import CreateExamForm from "./create-exam-form.component";
 
 const CreateExamPage = () => {
-  const [needsPassword, setNeedsPassword] = useState(false);
   return (
     <div className="d-flex flex-row">
       <Sidebar />
       <div className="flex-grow-1">
         <Container className="my-3">
-          <Form>
-            <Row className="mt-3">
-              <Col md={6} xl={3}>
-                <TextInput
-                  label="Exam Name"
-                  id="exam-name"
-                  inputProps={{ placeholder: "Exam Name" }}
-                />
-              </Col>
-            </Row>
-            <Row className="mt-3">
-              <Col>
-                <TextareaInput
-                  label="Exam Description"
-                  id="exam-description"
-                  inputProps={{
-                    placeholder: "Exam Description",
-                    rows: 3,
-                    style: { resize: "none" },
-                  }}
-                />
-              </Col>
-            </Row>
-            <Row className="mt-3">
-              <Col>
-                <TextInput
-                  label="Exam's Start"
-                  id="exams-start"
-                  inputProps={{
-                    placeholder: "Exam's Start",
-                  }}
-                />
-              </Col>
-              <Col>
-                <TextInput
-                  label="Exam's End"
-                  id="exams-end"
-                  inputProps={{
-                    placeholder: "Exam's End",
-                  }}
-                />
-              </Col>
-            </Row>
-            <Row className="mt-3">
-              <Col md={6} xl={3}>
-                <NumberInput
-                  label="Total Score"
-                  id="total-score"
-                  inputProps={{
-                    placeholder: "Total Score",
-                  }}
-                />
-              </Col>
-            </Row>
-            <Row className="mt-3">
-              <Col>
-                <CheckboxInput
-                  label="Confirmation Required?"
-                  id="confirmation-required"
-                />
-              </Col>
-            </Row>
-            <Row className="mt-3">
-              <Col xs={12} md={6} xl={3}>
-                <CheckboxInput
-                  onChange={(e) => setNeedsPassword(e.target.checked)}
-                  label="Needs Password?"
-                  id="needs-password"
-                />
-              </Col>
-              {needsPassword && (
-                <Col xs={12} md={6} xl={4}>
-                  <TextInput
-                    label="Password"
-                    id="exam-password"
-                    inputProps={{ placeholder: "Exam's Password" }}
-                  />
-                </Col>
-              )}
-            </Row>
-            <Row className="mt-3">
-              <Col>
-                <Button> Create </Button>
-              </Col>
-            </Row>
-          </Form>
+          <CreateExamForm />
         </Container>
       </div>
     </div>
