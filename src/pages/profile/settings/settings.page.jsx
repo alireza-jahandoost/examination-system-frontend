0c8d6acc463d0f1a7cb732637af94dcb623bf732
alireza <alireza.jhd2000@gmail.com>
2021-11-25 18:28:57 +0330

    Style change password section

--- src/pages/profile/settings/settings.page.jsx
+++ src/pages/profile/settings/settings.page.jsx
@@ -29,37 +29,38 @@ const SettingsPage = () => {
           <div>
             <Row>
               <Col md={8} lg={6}>
-                <h2> Change Password </h2>
-                <Form onSubmit={handleSubmit}>
-                  <PasswordInput
-                    label="Current Password"
-                    error={errors.current_password}
-                    value={currentPassword}
-                    id="current-password"
-                    placeholder="Enter your current password"
-                    onChange={(e) => setCurrentPassword(e.target.value)}
-                  />
-                  <PasswordInput
-                    label="New Password"
-                    error={errors.password}
-                    value={newPassword}
-                    id="new-password"
-                    placeholder="Enter your new password"
-                    onChange={(e) => setNewPassword(e.target.value)}
-                  />
-                  <PasswordInput
-                    label="Confirm New Password"
-                    error={errors.password_confirmation}
-                    value={confirmNewPassword}
-                    id="confirm-new-password"
-                    placeholder="Enter your new password again"
-                    onChange={(e) => setConfirmNewPassword(e.target.value)}
-                  />
-                  <Button disabled={isLoading} type="submit">
-                    {" "}
-                    {isLoading ? "Loading..." : "Change Password"}{" "}
-                  </Button>
-                </Form>
+                <div className="text-start">
+                  <h2> Change Password </h2>
+                  <Form onSubmit={handleSubmit}>
+                    <PasswordInput
+                      label="Current Password"
+                      error={errors.current_password}
+                      value={currentPassword}
+                      id="current-password"
+                      placeholder="Enter your current password"
+                      onChange={(e) => setCurrentPassword(e.target.value)}
+                    />
+                    <PasswordInput
+                      label="New Password"
+                      error={errors.password}
+                      value={newPassword}
+                      id="new-password"
+                      placeholder="Enter your new password"
+                      onChange={(e) => setNewPassword(e.target.value)}
+                    />
+                    <PasswordInput
+                      label="Confirm New Password"
+                      error={errors.password_confirmation}
+                      value={confirmNewPassword}
+                      id="confirm-new-password"
+                      placeholder="Enter your new password again"
+                      onChange={(e) => setConfirmNewPassword(e.target.value)}
+                    />
+                    <Button disabled={isLoading} className="my-3" type="submit">
+                      {isLoading ? "Loading..." : "Change Password"}
+                    </Button>
+                  </Form>
+                </div>
               </Col>
             </Row>
           </div>
