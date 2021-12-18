ba9d5bcbffeba80f6f85bf66f8dbef6a51bd1683
alireza <alireza.jhd2000@gmail.com>
2021-12-18 15:36:32 +0330

    Change search bar in index all exams

--- src/pages/exams/index/index-all-exams/search.component.jsx
+++ src/pages/exams/index/index-all-exams/search.component.jsx
@@ -1,28 +1,43 @@
-import { InputGroup, Button, FormControl, Row, Col } from "react-bootstrap";
+import {
+  InputGroup,
+  Form,
+  Button,
+  FormControl,
+  Row,
+  Col,
+} from "react-bootstrap";
 import { BsSearch } from "react-icons/bs";
 
 const Search = ({ value, changeValue, handleClick }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleClick();
+  };
+
   return (
     <Row>
       <Col lg={2}></Col>
       <Col lg={8}>
-        <InputGroup size="lg" role="search" className="mb-3">
-          <FormControl
-            placeholder="Search Exam"
-            aria-label="search-exam"
-            aria-describedby="search-exam"
-            value={value}
-            onChange={(e) => changeValue(e.target.value)}
-          />
-          <Button
-            onClick={handleClick}
-            variant="outline-secondary"
-            title="search exam"
-            id="search-exam"
-          >
-            <BsSearch />
-          </Button>
-        </InputGroup>
+        <Form onSubmit={handleSubmit}>
+          <InputGroup size="lg" role="search" className="mb-3">
+            <FormControl
+              className="rounded-pill"
+              placeholder="Search Exam"
+              aria-label="search-exam"
+              aria-describedby="search-exam"
+              value={value}
+              onChange={(e) => changeValue(e.target.value)}
+            />
+            <Button
+              type="submit"
+              title="search exam"
+              id="search-exam"
+              variant="muted"
+            >
+              <BsSearch />
+            </Button>
+          </InputGroup>
+        </Form>
       </Col>
       <Col lg={2}></Col>
     </Row>
