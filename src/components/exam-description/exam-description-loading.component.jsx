c2b01094ebd66c642e4aadafbc22eededfc6fa3f
alireza <alireza.jhd2000@gmail.com>
2022-01-12 15:09:53 +0330

    Remove exam description component

--- src/components/exam-description/exam-description-loading.component.jsx
+++
@@ -1,9 +0,0 @@
-const ExamDescriptionLoading = () => {
-  return (
-    <div className="d-flex justify-content-center align-items-center">
-      <div>Loading...</div>
-    </div>
-  );
-};
-
-export default ExamDescriptionLoading;
