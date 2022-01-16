a45e84c96a71618c9171762109b099b6e851036c
alireza <alireza.jhd2000@gmail.com>
2022-01-16 17:06:13 +0330

    Update states sequentially

--- src/contexts/edit-question-context/edit-question.context.jsx
+++ src/contexts/edit-question-context/edit-question.context.jsx
@@ -93,144 +93,118 @@ export const EditQuestionProvider = ({ children, examId, questionId }) => {
     throwError,
   ]);
 
-  const updateQuestion = ({
+  const updateQuestion = async ({
     question_text,
     question_score,
     deletedStateIds,
     changedStates,
     createdStates,
   }) => {
-    setIsLoading(true);
-    const bodyOfRequest = {};
-    if (question_text) {
-      bodyOfRequest.question_text = question_text;
-    }
-    if (question_score) {
-      bodyOfRequest.question_score = question_score;
-    }
-    const requests = [];
-    const questionChanged = Object.keys(bodyOfRequest).length !== 0;
-    if (questionChanged) {
-      requests.push(
-        questionsUpdateRequest(examId, questionId, bodyOfRequest, token)
-      );
-    }
-    if (deletedStateIds) {
-      deletedStateIds.forEach((deletedStateId) => {
-        requests.push(
-          statesDeleteRequest(examId, questionId, deletedStateId, token)
+    try {
+      setIsLoading(true);
+      const bodyOfRequest = {};
+      if (question_text) {
+        bodyOfRequest.question_text = question_text;
+      }
+      if (question_score) {
+        bodyOfRequest.question_score = question_score;
+      }
+      const questionChanged = Object.keys(bodyOfRequest).length !== 0;
+      if (questionChanged) {
+        const questionResponse = await questionsUpdateRequest(
+          examId,
+          questionId,
+          bodyOfRequest,
+          token
         );
-      });
-    }
-    if (createdStates) {
-      createdStates.forEach((createdState) => {
-        const createdStateBody = {};
-        if (createdState.integer_part !== undefined) {
-          createdStateBody.integer_part = createdState.integer_part;
+        const { question: receivedQuestion } = questionResponse.data.data;
+        setQuestion(receivedQuestion);
+      }
+      if (deletedStateIds) {
+        for (const deletedStateId of deletedStateIds) {
+          await statesDeleteRequest(examId, questionId, deletedStateId, token);
         }
-        if (createdState.text_part !== undefined) {
-          createdStateBody.text_part = createdState.text_part;
+      }
+      const newStates =
+        deletedStateIds && deletedStateIds.length > 0
+          ? [
+              ...states.filter((state) =>
+                deletedStateIds.includes(state.id) ? false : true
+              ),
+            ]
+          : [...states];
+      if (createdStates) {
+        for (const createdState of createdStates) {
+          const createdStateBody = {};
+          if (createdState.integer_part !== undefined) {
+            createdStateBody.integer_part = createdState.integer_part;
+          }
+          if (createdState.text_part !== undefined) {
+            createdStateBody.text_part = createdState.text_part;
+          }
+          const createdStateResponse = await statesStoreRequest(
+            examId,
+            questionId,
+            createdStateBody,
+            token
+          );
+          const { state: currentState } = createdStateResponse.data.data;
+          newStates.push({
+            text_part: currentState.text_part,
+            integer_part: currentState.integer_part,
+            id: currentState.state_id,
+          });
         }
-        requests.push(
-          statesStoreRequest(examId, questionId, createdStateBody, token)
-        );
-      });
-    }
-    if (changedStates) {
-      changedStates.forEach((changedState) => {
-        const changedStateBody = {};
-        if (changedState.integer_part !== undefined) {
-          changedStateBody.integer_part = changedState.integer_part;
-        }
-        if (changedState.text_part !== undefined) {
-          changedStateBody.text_part = changedState.text_part;
-        }
-        requests.push(
-          statesUpdateRequest(
+      }
+      if (changedStates) {
+        for (const changedState of changedStates) {
+          const changedStateBody = {};
+          if (changedState.integer_part !== undefined) {
+            changedStateBody.integer_part = changedState.integer_part;
+          }
+          if (changedState.text_part !== undefined) {
+            changedStateBody.text_part = changedState.text_part;
+          }
+          const changeStateResponse = await statesUpdateRequest(
             examId,
             questionId,
             changedState.id,
             changedStateBody,
             token
-          )
-        );
-      });
-    }
-    axios
-      .all(requests)
-      .then(
-        axios.spread((...responses) => {
-          if (isMounted()) {
-            let start = 0,
-              end = responses.length - 1;
-            if (questionChanged) {
-              const questionResponse = responses[0];
-              const { question: receivedQuestion } = questionResponse.data.data;
-              setQuestion(receivedQuestion);
-              start = 1;
-            }
+          );
 
-            const statesWithoutRemovedStates = deletedStateIds
-              ? [
-                  ...states.filter((state) => {
-                    if (deletedStateIds.includes(state.id)) {
-                      return false;
-                    }
-                    return true;
-                  }),
-                ]
-              : [...states];
-            start += deletedStateIds ? deletedStateIds.length : 0;
-            if (createdStates) {
-              for (let i = start; i < start + createdStates.length; i++) {
-                const { state: createdState } = responses[i].data.data;
-                statesWithoutRemovedStates.push({
-                  text_part: createdState.text_part,
-                  integer_part: createdState.integer_part,
-                  id: createdState.state_id,
-                });
-              }
-              start += createdStates.length;
-            }
+          const { state: receivedState } = changeStateResponse.data.data;
 
-            for (let i = start; i <= end; i++) {
-              const { state: changedState } = responses[i].data.data;
-              for (let j = 0; j < statesWithoutRemovedStates.length; j++) {
-                if (
-                  statesWithoutRemovedStates[j].id === changedState.state_id
-                ) {
-                  statesWithoutRemovedStates[j].text_part =
-                    changedState.text_part;
-                  statesWithoutRemovedStates[j].integer_part =
-                    changedState.integer_part;
-                }
-              }
-            }
-
-            setStates(statesWithoutRemovedStates);
-            setIsLoading(false);
-            setErrors({});
-          }
-        })
-      )
-      .catch((errors) => {
-        if (isMounted()) {
-          switch (Number(errors?.response?.status)) {
-            case 401:
-              removeUserInfo();
-              break;
-            case 422:
-              const { message, errors: receivedErrors } = errors.response.data;
-              setErrors({ message, ...receivedErrors });
-              break;
-            default:
-              setErrors({
-                message: "something went wrong, please try again later",
-              });
-          }
-          setIsLoading(false);
+          const currentStateIndex = newStates.findIndex(
+            (state) => state.id === receivedState.state_id
+          );
+          newStates[currentStateIndex].text_part = receivedState.text_part;
+          newStates[currentStateIndex].integer_part =
+            receivedState.integer_part;
         }
-      });
+      }
+
+      setStates(newStates);
+      setIsLoading(false);
+      setErrors({});
+    } catch (errors) {
+      if (isMounted()) {
+        switch (Number(errors?.response?.status)) {
+          case 401:
+            removeUserInfo();
+            break;
+          case 422:
+            const { message, errors: receivedErrors } = errors.response.data;
+            setErrors({ message, ...receivedErrors });
+            break;
+          default:
+            setErrors({
+              message: "something went wrong, please try again later",
+            });
+        }
+        setIsLoading(false);
+      }
+    }
   };
 
   const deleteQuestion = async () => {
